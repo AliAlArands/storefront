3de0ba14a64d4093bcf013286012d8c8bd0ddd60
Ali Darwish <alidarwish502@gmail.com>
2022-07-02 07:32:04 +0300

    Built the data model for product reviews.

--- store/models.py
+++ store/models.py
@@ -5,6 +5,7 @@ from uuid import uuid4
 
 # Create your models here.
 
+
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
     discount = models.FloatField()
@@ -17,6 +18,7 @@ class Collection(models.Model):
 
     def __str__(self):
         return self.title
+
 
 class Product (models.Model):
     title = models.CharField(max_length=255)
@@ -33,6 +35,7 @@ class Product (models.Model):
     def __str__(self) -> str:
         return self.title
 
+
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
     MEMBERSHIP_SILVER = 'S'
@@ -42,7 +45,8 @@ class Customer(models.Model):
         (MEMBERSHIP_GOLD, 'Gold'),
         (MEMBERSHIP_SILVER, 'Silver'),
     ]
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(
@@ -51,11 +55,13 @@ class Customer(models.Model):
     def __str__(self) -> str:
         return f'{self.user.first_name} {self.user.last_name}'
 
+
 class Address (models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.ForeignKey(
         Customer, on_delete=models.CASCADE)
+
 
 class Order (models.Model):
     PENDEING_PAYMENT = 'P'
@@ -71,21 +77,35 @@ class Order (models.Model):
         max_length=1, choices=PAYMENT_STATUS, default=PENDEING_PAYMENT)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
+
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name='items')
+    order = models.ForeignKey(
+        Order, on_delete=models.PROTECT, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
+
 class Cart(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4) #for not allowing a hacker to modify our carts.
+    # for not allowing a hacker to modify our carts.
+    id = models.UUIDField(primary_key=True, default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    cart = models.ForeignKey(
+        Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
 
     class Meta:
-        unique_together = [['cart','product']] # for not allowing duplicate values.
+        # for not allowing duplicate values.
+        unique_together = [['cart', 'product']]
+
+
+class Review(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='reviews')
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateField(auto_now_add=True)
