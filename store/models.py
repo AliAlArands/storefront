810f27dac13fbe15578b9edc0242dfdd71741758
Ali Darwish <alidarwish502@gmail.com>
2022-07-02 07:19:33 +0300

    Build the data model for cart and cart items.

--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 from django.conf import settings
+from uuid import uuid4
 
 # Create your models here.
 
@@ -75,3 +76,16 @@ class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4) #for not allowing a hacker to modify our carts.
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+
+    class Meta:
+        unique_together = [['cart','product']] # for not allowing duplicate values.
