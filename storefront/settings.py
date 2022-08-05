82dfc0ec177f82b98cdeffcdfcc80ab490fff5db
Ali Darwish <alidarwish502@gmail.com>
2022-08-05 16:53:57 +0300

    Changed the life time of the access token.

--- storefront/settings.py
+++ storefront/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -63,6 +64,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
+    'ACCESS_TOKEN_LIFETIME':timedelta(minutes=50)
 }
 
 DJOSER = {
